f0fb1d12ecbabac777a1e336b6a443778bd9e9e1
Luigi Campanaro <luigicampanaror@gmail.com>
2025-07-04 09:36:54 +0200

    fix: building from scratch every time

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ export BUILDKIT_PROGRESS=plain
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-docker compose -f $SCRIPT_DIR/docker-compose.yml build 
+docker compose -f $SCRIPT_DIR/docker-compose.yml build --no-cache
